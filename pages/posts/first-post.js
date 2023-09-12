d9391e8873d890e6754f7786ac316af4b12ce2fc
itachi-P <itachip38@gmail.com>
2023-09-12 13:40:18 +0900

    Change image radio

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -10,7 +10,7 @@ export default function FirstPost() {
                 <title>First Post</title>
             </Head>
             <h1>I'm sorry. This page is still under construction.</h1>
-            <Image src="/images/nante-cotta.jpg"  height={500} width={500} alt="nante-cotta" />
+            <Image src="/images/nante-cotta.jpg" width={600} height={400} alt="nante-cotta" />
         </Layout>
     );
 }
