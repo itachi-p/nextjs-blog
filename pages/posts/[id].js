e28ba62cf8d715455d31178013646ccb9508904d
itachi-P <itachip38@gmail.com>
2023-09-14 23:53:30 +0900

    style:Polishing the Post Page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,10 +1,8 @@
-// ダイナミックルートを使用する場合、ファイル名を[xxx].jsとする必要がある
-// リクエストの度に読み込んだファイルを解析して'id'(ファイル名)を取得し、
-// そのidをブラウザ上のURL子要素として動的にページリンクが生成される
-//※ただし実際にidに対応するHTMLファイルは存在しないため、再読み込みするとエラーになる
-
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
 
 export async function getStaticProps({ params }) {
     const postData = await getPostData(params.id);
@@ -26,13 +24,16 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
     return (
         <Layout>
-            {postData.title}
-            <br />
-            {postData.id}
-            <br />
-            {postData.date}
-            <br />
-            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+            <article>
+                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+                <div className={utilStyles.lightText}>
+                    <Date dateString={postData.date} />
+                </div>
+                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            </article>
         </Layout>
     );
 }
