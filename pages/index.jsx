2f45550892b89ff835a9e57d51a1e7e4248ab0a4
itachi-P <itachip38@gmail.com>
2023-09-15 11:30:23 +0900

    Recreate netlify.toml

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,6 @@ import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 import Image from 'next/image';
-import Date from '../components/date';
 import Footer from '../components/footer';
 
 export default function Home() {
@@ -14,15 +13,15 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Hi, I'm <strong>itachi-p</strong>.</p>
-        <p>I love animals🐻🐯🐱🦆🐸🪲 and like vegetable curry🍛.</p>
+        <p>I love animals🐻🐯🐱🦆🐸🦎🪲 and like vegetable curry🍛.</p>
         <p>This page is <strong><Link href="https://github.com/itachi-p/nextjs-blog">the repository</Link></strong> on my GitHub.</p>
         <p>The source for this site is {' '}
           <Link href="https://nextjs.org/learn">Next.js tutorial</Link>.
         </p>
       </section>
-      <section  className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+      <section>
         <h2 className={utilStyles.headingLg}>Contents:</h2>
-        <ul className={utilStyles.list}>
+        <ul>
           <li>
             Read{' '}
             <Link href={`/posts/blog-top`}>
@@ -49,8 +48,6 @@ export default function Home() {
               ビンタですと(？)🐧👏
               <Image src="/images/penguinshibaki.gif" height={144} width={144} alt="🐧👏" />
             </Link>
-            <small className={utilStyles.lightText}>
-              </small>
           </li>
         </ul>
       </section>
