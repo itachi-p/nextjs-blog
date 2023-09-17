651f0209d9f7d69f8b050ef29e4d0c7c21abdf4a
itachi-P <itachip38@gmail.com>
2023-09-17 11:53:31 +0900

    style:Complete users and each user page once

--- pages/api/users/index.jsx
+++ pages/api/users/index.jsx
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-    // API Routes中の処理はサーバ側なので、下のconsole.log()はサーバ側のコンソールにだけ出力される
+    // API Routes中の処理はサーバ側なので、下のconsole.log()はサーバ側のコンソールに出力される
     console.log(req.method);
     const response = await fetch('https://jsonplaceholder.typicode.com/users/');
     const users = await response.json();
--- pages/users.jsx
+++ pages/users.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
+import styles from '../components/layout.module.css';
 
-export default function Home() {
+export default function Users() {
     const [users, setUsers] = useState([]);
     useEffect(() => {
         const fetchUsers = async () => {
@@ -13,16 +14,25 @@ export default function Home() {
     }, []);
 
     return (
-        <div>
+        <div className={styles.container}>
+            <h2>Users</h2>
+            <hr />
+            <p><span>※データは<Link href="https://www.jsonplaceholder.org/">JSONPlaceholder</Link>のサンプルAPIを利用</span></p>
+            <p>ユーザ一覧と、各ユーザidに紐づく単体データ</p>
+            <p>各ユーザの詳細ページは<Link href="https://ja.wikipedia.org/wiki/%E8%AE%83%E5%B2%90%E3%81%86%E3%81%A9%E3%82%93">元祖てぬきうどん</Link>方式</p>
+            <p>(/api/users/[id]のJSONをそのまま表示)</p>
             <ul>
                 {users.map((user) => (
                     <li key={user.id}>
-                        <Link href={`/users/${user.id}`}>
+                        <Link href={`api/users/${user.id}`}>
                             {user.name}
                         </Link>
                     </li>
                 ))}
             </ul>
+            <div className={styles.backToHome}>
+                <Link href="/">← Back to home</Link>
+            </div>
         </div>
     );
 }
